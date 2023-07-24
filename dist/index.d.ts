996728a91174102587e7f25768c7e6b0aa61fae6
Wyatt Ades <me@wyattades.com>
2023-07-23 23:27:19 -0400

    update dist

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,5 @@
 import { PluginListenerHandle } from '@capacitor/core';
-import firebase from 'firebase/app';
+import firebase from 'firebase/compat/app';
 
 type EventMapping = {
     cfaSignInPhoneOnCodeSent: {
